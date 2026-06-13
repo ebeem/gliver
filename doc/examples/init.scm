6068269b06624ae175af667dacd68fe2eecef7ad
ebeem <ibeem@outlook.com>
2026-06-13 11:24:58 +0300

    add small example to docs

--- doc/examples/init.scm
+++ doc/examples/init.scm
@@ -1,1 +1,20 @@
+;;; doc/examples/init.scm --- Example init file for Gliver
 
+(use-modules (gliver core)
+             (gliver commands)
+			 (gliver contrib ui which-key)
+			 (gliver contrib utils systemd)
+			 (gliver contrib keybindings sway))
+
+(set! *terminal* "foot")
+;; set log level for better debugging
+;; (log-level-set! 'debug)
+(log-info "Loading init.scm configuration")
+
+;; clear default keybindings and add install sway's keybindings
+(keybindings-clear!)
+(keybindings-sway-install-default!)
+
+;; define an additional keybinding to spawn fuzzel with super+o
+(define-key *top-map* "s-o"
+  (lambda () (cmd-exec (format #f "exec fuzzel"))))
