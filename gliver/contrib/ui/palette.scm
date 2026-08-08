a7e51ef8c722defc854c764bcbc0356ea96425e3
ebeem <ibeem@outlook.com>
2026-08-08 06:51:03 +0300

    add palette kill command to the palette interface

--- gliver/contrib/ui/palette.scm
+++ gliver/contrib/ui/palette.scm
@@ -3,7 +3,9 @@
   #:export (
 			*palette-backend*
 			*palette-dmenu-options*
+			*palette-backend-kill*
 			palette-show
+			palette-kill
 			make-dmenu-options
 ))
 
@@ -17,16 +19,24 @@ make-launcher-backend.")
 palette is called. This should be any function that will call
 make-launcher-backend.")
 
+(define-var *palette-backend-kill* #f
+			"The command to kill palette menu.")
+
 (define* (palette-show candidates #:key (keys '())
 					   (theme-overrides '()) (backend *palette-backend*))
   "Prompt the user to select from CANDIDATES.
 BACKEND defaults to the globally configured `*palette-backend*`.
 Returns (index . original-candidate) or #f if cancelled."
+  (palette-kill)
   (let ((idx (backend candidates
 					  #:theme-overrides theme-overrides)))
 	(if (and idx (> (length keys) idx))
 		(list-ref keys idx)
 		idx)))
+
+(define* (palette-kill #:key (backend-kill *palette-backend-kill*))
+  "Kill the backend process."
+  (backend-kill))
 
 (define* (make-dmenu-options options colors #:key (pango #t)
 							 (widths '()) (searchable '()) (visible '()))
