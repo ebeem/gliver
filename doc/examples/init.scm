4351774f918b1ebf5d78679da55e91a18fd97799
ebeem <ibeem@outlook.com>
2026-08-11 05:50:07 +0300

    update initial example and add rofi and foot as optional dependencies

--- doc/examples/init.scm
+++ doc/examples/init.scm
@@ -1,20 +1,29 @@
-;;; doc/examples/init.scm --- Example init file for Gliver
+;;; Example Gliver Configuration
+;;; ~/.config/gliver/init.scm
+;;;
+;;; This file is loaded by Gliver at startup.  It is a Guile Scheme
+;;; program with access to all Gliver APIs.
 
 (use-modules (gliver core)
              (gliver contrib commands)
-			 (gliver contrib ui which-key)
+			 (gliver contrib layout alternating)
 			 (gliver contrib utils systemd)
-			 (gliver contrib ui integrations rofi)
-			 (gliver contrib keybindings gliver))
+			 (gliver contrib keybindings gliver)
+			 (gliver contrib ui which-key)
+			 (gliver contrib ui integrations rofi))
 
 (set! *terminal* "foot")
-;; set log level for better debugging
-;; (log-level-set! 'debug)
+(set! *theme-font* "Iosevka Nerd Font Bold")
+(log-level-set! 'info)
 (log-info "Loading init.scm configuration")
 
-;; clear default keybindings and add install sway's keybindings
+;; install gliver default keybindings (you can try out sway, stumpwm, etc)
 (keybindings-clear!)
 (keybindings-gliver-install-default!)
 
 ;; install rofi as dmenu and app launcher (you can replace it with fuzzel, fzf, etc)
 (rofi-install-all!)
+
+;; enable which-key module
+(which-key-enable!)
+
