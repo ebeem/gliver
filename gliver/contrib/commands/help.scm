9d4f20fc24f9e4985c2480768ee862554a77e26c
ebeem <ibeem@outlook.com>
2026-09-07 17:40:40 +0300

    use new palette APIs to describe commands, variables, and keys

--- gliver/contrib/commands/help.scm
+++ gliver/contrib/commands/help.scm
@@ -6,6 +6,7 @@
 (define-module (gliver contrib commands help)
   #:use-module (gliver core)
   #:use-module (ice-9 string-fun)
+  #:use-module (ice-9 pretty-print)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-69)
   #:use-module (gliver contrib ui palette)
@@ -16,54 +17,123 @@
 			describe-command
 			where-is
 			list-commands
+			safe-var-value-string
 ))
 
-;;; help / introspection commands
+(define (safe-var-value-string val)
+  "Return a compact, safe string representation of VAL for display in palette and logs."
+  (cond
+   ((string? val)
+    (if (> (string-length val) 60)
+        (string-append (substring val 0 57) "...")
+        (format #f "~s" val)))
+   ((or (number? val) (boolean? val) (symbol? val) (char? val) (null? val))
+    (format #f "~a" val))
+   (else
+    (catch #t
+      (lambda ()
+        (call-with-output-string
+          (lambda (p) (truncated-print val p #:width 60))))
+      (lambda _
+        (catch #t
+          (lambda ()
+            (let ((s (format #f "~a" val)))
+              (if (> (string-length s) 60)
+                  (string-append (substring s 0 57) "...")
+                  s)))
+          (lambda _ "#<object>")))))))
 
+;;; help / introspection commands
 (define-command (describe-variable)
   "Display the documentation of variables."
-  (let ((selected (palette-show
-				   (hash-table-fold *variable-registry*
-									(lambda (name val-pair acc)
-									  (let ((module (car val-pair))
-											(docstring (string-replace-substring (cdr val-pair) "\n" " ")))
-										(cons (make-dmenu-options
-											   (list (list " " name (var-get name) (module-name module) docstring))
-											   (list *palette-icon-color* *palette-name-color* *palette-value-color* *palette-help-color* *palette-doc-color*)
-											   #:widths *palette-variables-widths*
-											   #:searchable *palette-variables-searchable*
-											   #:visible *palette-variables-visible*)
-											  acc)))
-									'())
-				   #:keys (hash-table-keys *variable-registry*))))
-	;; TODO: show a message with information about the variable
-	;; and allow editing its value
-	(log-info selected)))
+  (let* ((keys-and-entries
+          (hash-table-fold *variable-registry*
+                           (lambda (name val-pair acc)
+                             (let* ((module (car val-pair))
+                                    (val (catch #t (lambda () (var-get name)) (lambda _ "<unbound>")))
+                                    (safe-val (safe-var-value-string val))
+                                    (docstring (string-replace-substring (or (cdr val-pair) "") "\n" " "))
+                                    (opt (make-dmenu-options
+                                          (list (list " " name safe-val (module-name module) docstring))
+                                          (list *palette-icon-color* *palette-name-color* *palette-value-color* *palette-help-color* *palette-doc-color*)
+                                          #:widths *palette-variables-widths*
+                                          #:searchable *palette-variables-searchable*
+                                          #:visible *palette-variables-visible*)))
+                               (cons (cons name (if (pair? opt) (car opt) opt)) acc)))
+                           '()))
+         (sorted (sort keys-and-entries
+                       (lambda (a b)
+                         (string<? (symbol->string (car a))
+                                   (symbol->string (car b))))))
+         (keys (map car sorted))
+         (options (map cdr sorted)))
+    (palette-show options #:keys keys
+                  #:prompt "Describe variable: "
+                  #:on-select
+                  (lambda (selected)
+                    (when selected
+                      (let* ((rec (hash-table-ref/default *variable-registry* selected #f))
+                             (val (catch #t (lambda () (var-get selected)) (lambda _ "<unbound>")))
+                             (doc (and rec (cdr rec))))
+                        (log-info "Variable ~a = ~a~%~a" selected (safe-var-value-string val) (or doc "No documentation."))))))))
 
-(define-command (describe-key key-str)
-  #:interactive (string)
-  "Describe what a key binding does."
-  (let* ((key (kbd key-str))
-         (binding (lookup-key *root-map* key))
-         (action (and binding (gliver-binding-action binding))))
-    (if action
-        (cond
-         ((command? (command-find action))
-          (log-debug "~a → ~a: ~a" key-str action
-                   (command-docstring (command-find action))))
-         ((gliver-keymap? action)
-          (log-debug "~a → keymap: ~a" key-str (gliver-keymap-name action)))
-         (else
-          (log-debug "~a → ~a" key-str action)))
-        (log-debug "~a is not bound." key-str))))
+(define-command (describe-command)
+  "Describe a command, or prompt to select from all commands."
+  (let* ((keys-and-entries
+          (hash-table-fold *command-registry*
+                           (lambda (cname cmd acc)
+                             (let* ((doc (string-replace-substring (or (command-docstring cmd) "") "\n" " "))
+                                    (opt (make-dmenu-options
+                                          (list (list " " cname doc))
+                                          (list *palette-icon-color* *palette-name-color* *palette-doc-color*)
+                                          #:widths '(2 35 60)
+                                          #:searchable '(#f #t #t)
+                                          #:visible '(#t #t #t))))
+                               (cons (cons cname (if (pair? opt) (car opt) opt)) acc)))
+                           '()))
+         (sorted (sort keys-and-entries
+                       (lambda (a b)
+                         (string<? (symbol->string (car a))
+                                   (symbol->string (car b))))))
+         (keys (map car sorted))
+         (options (map cdr sorted)))
+    (palette-show options #:keys keys
+                  #:prompt "Describe command: "
+                  #:on-select
+                  (lambda (selected)
+                    (when selected
+                      (let ((cmd (command-find selected)))
+                        (when cmd
+                          (log-info "Command ~a: ~a" selected (command-docstring cmd)))))))))
 
-(define-command (describe-command name)
-  #:interactive (string)
-  "Describe a command."
-  (let ((cmd (command-find name)))
-    (if cmd
-        (log-debug "~a: ~a" name (command-docstring cmd))
-        (log-warn "Unknown command: ~a" name))))
+(define-command (describe-key)
+  "Describe a keybinding, prompting to select from all active keybindings."
+  (let* ((top-pairs (gliver-keymap->alist *top-map*))
+         (root-pairs (gliver-keymap->alist *root-map*))
+         (all-pairs (append top-pairs root-pairs))
+         (entries (map (lambda (pair)
+                         (let* ((key-str (gliver-key->string (car pair)))
+                                (action (gliver-binding-action (cdr pair)))
+                                (act-str (format #f "~a" action))
+                                (doc (if (or (symbol? action) (string? action))
+                                         (let ((cmd (command-find action)))
+                                           (if cmd (or (command-docstring cmd) "") ""))
+                                         ""))
+                                (opt (make-dmenu-options
+                                      (list (list " " key-str act-str doc))
+                                      (list *palette-icon-color* *palette-name-color* *palette-value-color* *palette-doc-color*)
+                                      #:widths '(2 20 25 50)
+                                      #:searchable '(#f #t #t #t)
+                                      #:visible '(#t #t #t #t))))
+                           (cons key-str (if (pair? opt) (car opt) opt))))
+                       all-pairs))
+         (sorted (sort entries (lambda (a b) (string<? (car a) (car b))))))
+    (palette-show (map cdr sorted) #:keys (map car sorted)
+                  #:prompt "Describe key: "
+                  #:on-select
+                  (lambda (selected)
+                    (when selected
+                      (log-info "Key ~a is bound in active keymap." selected))))))
 
 (define-command (where-is name)
   #:interactive (string)
@@ -79,11 +149,11 @@
     (if (null? bindings)
         (log-debug "~a is not on any key." name)
         (log-debug "~a is on ~a" name
-                 (string-join (map (lambda (b) (gliver-key->string (car b))) bindings)
-                              ", ")))))
+                   (string-join (map (lambda (b) (gliver-key->string (car b))) bindings)
+								", ")))))
 
 (define-command (list-commands)
   "List all available commands."
   (log-debug "~a" (string-join (sort (map symbol->string (command-all))
-                                   string<?)
-                             " ")))
+									 string<?)
+                               " ")))
